b08560675fb810322510075ba8f9254246a9cd9a
Simran <mdrsimran@gmail.com>
2020-11-16 23:56:09 -0600

    commit tree 93b9913e6f772cda256164fa442e280e8e9638de

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "Black", "innocent",1);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Brown", "calm",1);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "White", "goofy", 1);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 25, "M", "Black", "loyal", 0);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa" 15, "F", "Chocolate", "sweet", 1);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 8, "M", "Brown", "goofy", 1);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 19, "F", "Dark brown", "naive", 1);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 11, "M", "Golden", "agressive",0);
+
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
